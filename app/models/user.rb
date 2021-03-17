c60ade778a359ab8aaae8563425d314b232327f8
andre-tlima <andre-tlima@hotmail.com>
2021-03-17 20:29:55 +0000

    adicionando arquivos da unit 6

---
+++ app/models/user.rb
@@ -0,0 +1,21 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  validates_uniqueness_of :auth_token
+  before_create :generate_authentication_token!
+  has_many :gains, dependent: :destroy
+  has_many :expenses, dependent: :destroy
+
+  def info
+    "#{email} - #{created_at} - Token: #{Devise.friendly_token}"
+  end
+
+  def generate_authentication_token!
+    begin
+      self.auth_token = Devise.friendly_token
+    end while User.exists?(auth_token: auth_token)
+  end
+end
